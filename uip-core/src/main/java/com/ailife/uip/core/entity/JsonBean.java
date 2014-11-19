4e5dbabcf9c4f075fab17e6c76348b53724c5ca3
chenmm <zjgsuchen@163.com>
2014-11-19 10:45:40 +0800

    Update ID Generator. Redis first, then UUID.

--- uip-core/src/main/java/com/ailife/uip/core/entity/JsonBean.java
+++ uip-core/src/main/java/com/ailife/uip/core/entity/JsonBean.java
@@ -1,7 +1,35 @@
 package com.ailife.uip.core.entity;
 
+import com.ailife.uip.core.util.ReflectionUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.SimplePropertyPreFilter;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by chenmm6 on 2014/11/19.
+ * Created by chenmm6 on 2014/11/6.
  */
-public class JsonBean {
+public abstract class JsonBean<T> implements Json {
+
+    protected abstract T getT();
+
+    @Override
+    public String toJSON() {
+        Field[] fields = getT().getClass().getDeclaredFields();
+        List<String> list = new ArrayList<String>();
+        for (Field field : fields) {
+            if (ReflectionUtil.isBeanField(field) && ReflectionUtil.isPrimitiveOrString(field)) {
+                list.add(field.getName());
+            }
+        }
+        return JSONObject.toJSONString(getT(), new SimplePropertyPreFilter(list.toArray(new String[list.size()])));
+    }
+
+    protected static <T extends Json> T toBean(String json, Class<T> clz) {
+        return JSON.parseObject(json, clz);
+    }
+
 }
--- uip-doc/src/main/java/com/ailife/uip/doc/util/ID.java
+++ uip-doc/src/main/java/com/ailife/uip/doc/util/ID.java
@@ -1,19 +1,47 @@
 package com.ailife.uip.doc.util;
 
+import com.ailife.uip.core.util.RedisKey;
 import com.ailife.uip.core.util.Symbol;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.data.redis.core.BoundValueOperations;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
 import org.springframework.util.IdGenerator;
 
 /**
  * Created by chenmm6 on 2014/11/3.
  */
-public class ID {
+@Service
+public class ID implements ApplicationContextAware{
 
-	@Autowired
-	private static IdGenerator idGenerator;
+    private static final Long MIN = 1000000L;
+    private static final Long DELTA = 1L;
 
-	public static String getNewId() {
-		return idGenerator.generateId().toString().replaceAll(Symbol.MINUS, Symbol.EMPTY);
-	}
+    private static ApplicationContext atx;
+
+    @Autowired
+    private IdGenerator idGenerator;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        atx = applicationContext;
+    }
+
+    public static String getNewId() {
+        ID instance = atx.getBean(ID.class);
+        if (instance.stringRedisTemplate != null) {
+            BoundValueOperations<String, String> ops = instance.stringRedisTemplate.boundValueOps(RedisKey.ID_GENERATOR_KEY);
+            ops.setIfAbsent(String.valueOf(MIN));
+            return String.valueOf(ops.increment(DELTA));
+        } else {
+            return instance.idGenerator.generateId().toString().replaceAll(Symbol.MINUS, Symbol.EMPTY);
+        }
+    }
 
 }
